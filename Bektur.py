e817b2e951c1cbedbac717d47f5f6fa48caf515b
Bektur Saralinov <bektursaralinov@MacBook-Air-Bektur.local>
2022-06-13 17:33:58 +0600

    first commit

---
+++ Bektur.py
@@ -0,0 +1,8 @@
+f_num = float("Введите первое число>>")
+oper = input("Введите операцию")
+s_num = float("Введите второе число")
+if oper == "+":
+    print = (f_num + s_num)
+elif oper == "-":
+    print = (f_num - s_num)
+elif 
